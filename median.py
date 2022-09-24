4b5c8509b65fafdd2d18d756e96de49fb65efbad
muzami1ali <muzamil.ali@kcl.ac.uk>
2022-09-24 15:11:02 +0100

    Modify median.py to return median of the list

--- median.py
+++ median.py
@@ -1,6 +1,10 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+from operator import le
+
+
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -9,4 +13,15 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+numbers.sort()
+length = len(numbers)
+if length%2==0:
+    x = (length//2)
+    y = x - 1
+    z = (numbers[x] + numbers[y]) / 2
+else:
+    x = (length//2)
+    z = numbers[x]
+    
+print(z)
